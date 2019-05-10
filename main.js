48d047351fc367e92e6ff99ae8f4621765dd81b7
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-05-09 22:12:32 -0400

    begin to implement audio elements in dom

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
 // if array 0 is larger than array 15 alert tone
 // increment the array
 
-let stockValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let stockValue = [403, 402, 399, 400, 407, 399, 404, 405, 309, 350];
 let tones = ["a", "b", "c", "d", "e", "g", "a", "b", "c", "d", "e", "g"];
 let incrementor = 6;
+var synth = new Tone.Synth().toMaster();
 
-if (stockValue[0] > 1) {
-  incrementor++;
-  console.log(incrementor);
-} else {
-  incrementor--;
-  console.log(incrementor);
+// for (i = 0; i < stockValue.length; i++) {
+//   console.log(stockValue[i]);
+//   for (x = 1; x < stockValue.length; x++) {
+//     console.log(stockValue[x], stockValue[i]);
+//     if (stockValue[i] > stockValue[x]) {
+//       incrementor++;
+//       synth.triggerAttackRelease("C4", "16n");
+//       // console.log("up");
+//     } else {
+//       incrementor--;
+//       synth.triggerAttackRelease("B4", "16n");
+//       // console.log("down");
+//     }
+//     console.log(i);
+//   }
+// }
+
+const newArray = [];
+let secondArray = [];
+
+for (i = 0; i < stockValue.length; i++) {
+  newArray.push(stockValue[i]);
+  console.log(newArray);
 }
+
+for (x = 1; x < stockValue.length; x++) {
+  secondArray.push(stockValue[x]);
+  console.log(secondArray);
+}
+
+for (i = 0; i < stockValue.length; i++) {
+  // console.log(newArray[i], secondArray[i]);
+  if (newArray[i] > secondArray[i]) {
+    incrementor++;
+    console.log(incrementor);
+  } else {
+    incrementor--;
+    console.log(incrementor);
+  }
+}
+
+//create a synth and connect it to the master output (your speakers)
+// var synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+// synth.triggerAttackRelease("C4", "16n");
 
 // import audio files
 // const a = new Audio("./A1.wav");
