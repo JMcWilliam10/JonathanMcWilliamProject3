67e5f956cf86caecf5f282690d5dc541a52d846a
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-05-10 21:48:28 -0400

    add chartjs

--- main.js
+++ main.js
@@ -1,44 +1,79 @@
-// if array 0 is larger than array 15 alert tone
-// increment the array
-
-let stockValue = [403, 402, 399, 400, 407, 399, 404, 405, 309, 350];
-let incrementor = 6;
-const newArray = [];
+let app = {};
+const stockValue = [100, 150, 200, 250, 200, 300, 200, 250, 300, 350, 400];
+// const stockValue = [1, 2, 3, 4, 5, 6, 7];
+// const stockValue = [7, 6, 5, 4, 3, 2];
+let incrementor = 7;
+// const newArray = [];
 const secondArray = [];
 const auditoryChart = [];
 
-for (i = 0; i < stockValue.length; i++) {
-  newArray.push(stockValue[i]);
-}
-
+// Make arrays
+// for (i = 0; i < stockValue.length; i++) {
+//   newArray.push(stockValue[i]);
+// }
 for (x = 1; x < stockValue.length; x++) {
   secondArray.push(stockValue[x]);
 }
+// const stockValue2 = stockValue.shift();
 
+// function to
 function playAudio(key) {
-  // console.log(key);
   document.querySelector(`#${key}`).play();
 }
-
+// loop to compare array values and increment which tone file to call
 for (i = 0; i < stockValue.length; i++) {
-  if (newArray[i] > secondArray[i]) {
+  if (stockValue[i] < secondArray[i]) {
+    console.table(stockValue[i], secondArray[i]);
     incrementor++;
-    // console.log(`a${incrementor++}`);
     auditoryChart.push(`a${incrementor}`);
-  } else {
+  } else if (stockValue[i] > secondArray[i]) {
     incrementor--;
-    // console.log(`a${incrementor--}`);
     auditoryChart.push(`a${incrementor}`);
   }
-  // console.log(auditoryChart);
 }
-
 // Establish for loop + set time out to play array
-let offset = 0;
+let offset = 1000;
 auditoryChart.forEach(function(tone) {
   setTimeout(function() {
     console.log(tone);
     playAudio(tone);
   }, offset);
-  offset += 500;
+  offset += 1000;
+});
+
+// Chart js
+
+let ctx = document.getElementById("myChart").getContext("2d");
+let chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: stockValue,
+    datasets: [
+      {
+        label: "Auditory Charts",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: stockValue
+      }
+    ]
+  },
+
+  // Configuration options go here
+  options: {
+    scales: {
+      yAxes: [
+        {
+          gridLines: {
+            color: "#FFFFFF"
+          },
+          ticks: {
+            beginAtZero: false
+          }
+        }
+      ]
+    }
+  }
 });
